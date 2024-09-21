68126ff4a7a95da162e3aee8d39dadb2684d2d69
jzhang319 <jzhang319@gmail.com>
2024-09-21 01:49:13 -0400

    changed favicon default to package icon

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,10 +8,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head
-        rel="icon"
-        href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>📦</text></svg>"
-      />
+      <head>
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>📦</text></svg>"
+        />
+      </head>
       <body>{children}</body>
     </html>
   );
